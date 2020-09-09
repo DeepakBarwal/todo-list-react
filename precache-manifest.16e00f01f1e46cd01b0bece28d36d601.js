0063c4a241c93c00ee5014f0fedce5eb6461e5b4
Deepak Barwal <deepakbarwal98@gmail.com>
2020-09-09 18:53:20 +0530

    Updates

---
+++ precache-manifest.16e00f01f1e46cd01b0bece28d36d601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0659bcd02bcd183eaa6f57893501623b",
+    "url": "/DeepakBarwal/todo-list-react/index.html"
+  },
+  {
+    "revision": "7fdcaabf09232333b670",
+    "url": "/DeepakBarwal/todo-list-react/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "c8c48e5a1c01be60e908",
+    "url": "/DeepakBarwal/todo-list-react/static/js/2.17b792b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DeepakBarwal/todo-list-react/static/js/2.17b792b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdcaabf09232333b670",
+    "url": "/DeepakBarwal/todo-list-react/static/js/main.744e8192.chunk.js"
+  },
+  {
+    "revision": "d4d676604bd597f43337",
+    "url": "/DeepakBarwal/todo-list-react/static/js/runtime-main.cf99f297.js"
+  }
+]);
